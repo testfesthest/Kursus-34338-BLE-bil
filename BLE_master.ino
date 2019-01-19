14b8272a5cff62acb8031f1f88b9be63ec3987d3
testfesthest <46591993+testfesthest@users.noreply.github.com>
2019-01-19 19:11:37 +0100

    before changing control method

--- BLE_master.ino
+++ BLE_master.ino
@@ -17,17 +17,19 @@
 AltSoftSerial BTserial; 
 // https://www.pjrc.com/teensy/td_libs_AltSoftSerial.html
 
+// Define global variables and constants
 char c=' ';
 boolean NL = true;
 int state = 0; //bruges ikke længere tror jeg
 const int SW_pin = 2; // digital pin connected to switch output
 const int X_pin = 0; // analog pin connected to X output
 const int Y_pin = 1; // analog pin connected to Y output
+int carMode = 0; // each mode (number) corresponds to a certain speed and direction for the car.
 
 void setup() 
 {
     pinMode(7, INPUT);
-    Serial.begin(9600);
+    Serial.begin(9600); //opens serial port, sets data rate to 9600 bps
     Serial.print("Sketch:   ");   Serial.println(__FILE__);
     Serial.print("Uploaded: ");   Serial.println(__DATE__);
     Serial.println(" ");
@@ -53,7 +55,10 @@ void loop()
     int x = map(analogRead(X_pin),0,1023,250,-250);
     int y = map(analogRead(Y_pin),0,1023,250,-250);
     //int pushButton = digitalRead(SW_pin);
-    //Serial.print(String(x)+"  "+String(y)+"\n");
+    
+    Serial.print(String(x)+"  "+String(y)+"\n");
+    BTserial.print(x); //print() converts it to binary (a byte) for us. Else use write().
+    delay(100);
     //Serial.print(x);
 
     //OBS.: if there are problems with connecting etc, then use the reset code for each in the sticky
@@ -62,7 +67,29 @@ void loop()
       // send one asci code each iteration. What you must do instead is send ONE characer/number (doesn't matter
       // but can be represented by asci code (right, confirm understanding with teacher)) and make switch cases here.
       // the more switch cases, the finer the steering can be. Maybe there is a way to send two at once, but forget about it for now.
-   //case(   
+      // you could also make code for slave module that waits until it has received all the characters by just using a
+      // certain end-line character, but no need for that.
+
+      // upload this to github and name "before changing data transfer code
+      // keep x and y map() to -255,255) - you cannot go over 255 if it must be contained in 1 byte, but can
+      // -255 be contained in one byte? try the code in the firetab that shows each byte received.
+      // Send the data by using:
+      // BTserial.print(y);
+      // BTserial.print(x);
+      // BTserial.print("/n");
+      // above could also be done in one string, but does not matter.
+      // Now the slave need three iterations to receive x and y, but it doesn't matter as it will only take 
+      // ~3ms with a 9600 baud rate. Increase baud rate for faster response.
+      // slave code: see the firefox tab for receiving the data
+      // slave code: for using the data use 
+      // Analog.write(motorpin1, y-x); Analog.write(motorpin2, y+x);
+
+      
+   // full speed forwards
+   if ((y <= 250 && y >= 245) && (x <= 10 && x >= -10))
+   {
+    
+   }
    // now the slave's version of this code needs to be combined with the motor code in "Car code" folder
       // there should be an amount of switc cases there equal to what is present here. Already somewhat made and
       // commented out
@@ -74,17 +101,17 @@ void loop()
    // lav billeder med fritzil
 }
 
-
+// Read from the Bluetooth module and send to the Arduino Serial Monitor
 void readFromModuleAndDisplayInMonitor()
 {
-    // Read from the Bluetooth module and send to the Arduino Serial Monitor
+    // send data only when you receive data    
     if (BTserial.available())
     {
+        // read the incoming byte
         c = BTserial.read();
         Serial.write(c); //println cannot be used, as c only contains 1 character at a time
     }
 }
-
 // Get what the user wrote int the serial monitor and send it to the BLE module
 // only really used for testing and setting configurations on it
 void readFromMonitorAndSendToModule()
